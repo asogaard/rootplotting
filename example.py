199734d98091f61f73b7619a70cfc75a57def9aa
asogaard <andreas.sogaard@gmail.com>
2017-04-25 17:43:50 +0200

    Update

--- example.py
+++ example.py
@@ -29,7 +29,13 @@ def main ():
     # – – – – – – – – – – – – – – – – – – – – – – – – – – – – – – – – – – – – – 
 
     # Load data
-    files  = glob.glob('data/*.root') #sys.argv[1:]
+    files  = glob.glob('data/*.root') 
+
+    if len(files) == 0:
+        warning("No files found. Try to run:")
+        warning(" $ source getSomeData.sh")
+        return
+
     tree   = 'BoostedJet+ISRgamma/Nominal/EventSelection/Pass/NumLargeRadiusJets/Postcut'
     prefix = 'Jet_'
 
@@ -52,7 +58,8 @@ def main ():
         pass
     
     msk_incl = (data['DSID'] >= 361039) & (data['DSID'] <= 361062)
-    msk_WZ   = (data['DSID'] >= 305435) & (data['DSID'] <= 305439)
+    msk_W    = (data['DSID'] >= 305435) & (data['DSID'] <= 305439)
+    msk_Z    = (data['DSID'] >= 305440) & (data['DSID'] <= 305445)
 
     
     # Plotting
@@ -64,8 +71,9 @@ def main ():
     c = ap.canvas(num_pads=2)
 
     # Add stacked backgrounds
-    h_WZ   = c.stack(data['m'][msk_WZ],   bins=bins, weights=data['weight'][msk_WZ],   fillcolor=ROOT.kRed + 1,   label='W/Z + #gamma', scale=1)
-    h_bkg  = c.stack(data['m'][msk_incl], bins=bins, weights=data['weight'][msk_incl], fillcolor=ROOT.kAzure + 7, label='Background')
+    h_W    = c.stack(data['m'][msk_W],    bins=bins, weights=data['weight'][msk_W],    fillcolor=ROOT.kAzure + 2, label='W(qq) + #gamma', scale=1)
+    h_Z    = c.stack(data['m'][msk_Z],    bins=bins, weights=data['weight'][msk_Z],    fillcolor=ROOT.kAzure + 3, label='Z(qq) + #gamma', scale=1)
+    h_bkg  = c.stack(data['m'][msk_incl], bins=bins, weights=data['weight'][msk_incl], fillcolor=ROOT.kAzure + 7, label='Incl. #gamma')
     
     # Draw stats. error of stacked sum
     h_sum  = c.getStackSum()
@@ -73,7 +81,7 @@ def main ():
     
     # Add (pseudo-) data
     np.random.seed(21)
-    h_data = c.plot (data['m'][msk_incl], bins=bins, weights=data['weight'][msk_incl] * (1. + np.random.randn(np.sum(msk_incl)) * 2.), markersize=0.8, label='Data')
+    h_data = c.plot (data['m'], bins=bins, weights=data['weight'] * (1. + np.random.randn(len(data['m'])) * 1.), markersize=0.8, label='Data', scale=1)
 
     # Draw error- and ratio plots
     h_err   = c.ratio_plot((h_sum,  h_sum), option='E2')
@@ -89,7 +97,7 @@ def main ():
 
     # Add line(s) and limit(s)
     c.yline(1, idx_pad=1)
-    c.ylim((0.5, 1.5), idx_pad=1)
+    c.ylim((0.8, 1.2), idx_pad=1)
 
     # Configure axis padding and -scale
     c.padding(0.35)
