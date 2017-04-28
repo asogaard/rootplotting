13426bd90af5b799de84d388e99f12802594bb0a
asogaard <andreas.sogaard@gmail.com>
2017-04-28 09:20:01 +0200

    Improve get-data script

--- getSomeData.sh
+++ getSomeData.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
-UNAME=
+# Target directory for files
+DIR=data
 
-if [ ! $UNAME ]; then
-    echo "No username provided"
+if [[ `echo $HOSTNAME | cut -c1-6` == "lxplus" ]]; then
+    # Running on lxplus; simply create symbolic link
+    rm -f ./$DIR
+    ln -s /eos/atlas/user/a/asogaard/Analysis/2016/BoostedJetISR/outputObjdef/2017-04-27 $DIR
+
 else
-    mkdir -p data
-    cd data/
-    scp -r ${UNAME}@lxplus.cern.ch:/afs/cern.ch/work/a/asogaard/public/Analysis/2016/BoostedJetISR/outputObjdef/objdef_MC_3610*.root .
-    scp -r ${UNAME}@lxplus.cern.ch:/afs/cern.ch/work/a/asogaard/public/Analysis/2016/BoostedJetISR/outputObjdef/objdef_MC_30543*.root .
-    scp -r ${UNAME}@lxplus.cern.ch:/afs/cern.ch/work/a/asogaard/public/Analysis/2016/BoostedJetISR/outputObjdef/objdef_MC_30544*.root .
-    cd -
+    # Running elsewhere; copy files
+    UNAME=
+    
+    if [ ! $UNAME ]; then
+	echo "Not running on lxplus; please provide a username (UNAME)"
+    else
+	mkdir -p $DIR
+	scp -r ${UNAME}@lxplus.cern.ch:/afs/cern.ch/work/a/asogaard/public/Analysis/2016/BoostedJetISR/outputObjdef/objdef_MC_3610*.root ./$DIR/
+	scp -r ${UNAME}@lxplus.cern.ch:/afs/cern.ch/work/a/asogaard/public/Analysis/2016/BoostedJetISR/outputObjdef/objdef_MC_30543*.root ./$DIR/
+	scp -r ${UNAME}@lxplus.cern.ch:/afs/cern.ch/work/a/asogaard/public/Analysis/2016/BoostedJetISR/outputObjdef/objdef_MC_30544*.root ./$DIR/
+    fi
+    
 fi
